3c8ad97da7576732569121189f6d5f079496af1b
Tom <twitchyliquid64@ciphersink.net>
2020-10-26 10:29:48 -0700

    Add mem->reg test

--- builder_test.go
+++ builder_test.go
@@ -36,6 +36,16 @@ func movImmediateByte(builder *Builder, reg int16, in int32) *obj.Prog {
 	return prog
 }
 
+func movMemByte(builder *Builder, intoReg, memReg int16) *obj.Prog {
+	prog := builder.NewProg()
+	prog.As = x86.AMOVB
+	prog.To.Type = obj.TYPE_REG
+	prog.To.Reg = intoReg
+	prog.From.Type = obj.TYPE_MEM
+	prog.From.Reg = memReg
+	return prog
+}
+
 func TestBasic(t *testing.T) {
 	b, _ := NewBuilder("amd64", 64)
 	b.AddInstruction(noop(b))
@@ -44,5 +54,15 @@ func TestBasic(t *testing.T) {
 	got, want := b.Assemble(), []byte{0x0f, 0x1f, 0xc0, 0xb0, 0x10, 0x04, 0x10}
 	if !bytes.Equal(got, want) {
 		t.Errorf("assembly = %v, want %v", got, want)
+	}
+}
+
+func TestMove(t *testing.T) {
+	b, _ := NewBuilder("amd64", 64)
+	b.AddInstruction(noop(b))
+	b.AddInstruction(movMemByte(b, x86.REG_AL, x86.REG_BX))
+	got, want := b.Assemble(), []byte{0x0F, 0x1F, 0xC0, 0x8A, 0x03}
+	if !bytes.Equal(got, want) {
+		t.Errorf("assembly = %v (%X), want %v", got, got, want)
 	}
 }
